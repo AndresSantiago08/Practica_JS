81b5cf45c7755b5151c620c02603e72d57a970b0
AndreSantiago08 <harper_hska_5@hotmail.com>
2022-03-16 22:40:35 -0600

    Update: Mostrar tipos

--- Pokedex.js
+++ Pokedex.js
@@ -23,7 +23,7 @@ function ConsultarAPI(){
             let DirImagenPokemon = Datos.sprites.front_default;
             MostrarImagen(DirImagenPokemon);
             EntraIntOrString(NombrePoke, Datos);
-
+            MostrarTipos(Datos);
         }
     });
 }
@@ -58,3 +58,180 @@ function MostrarNumero(NumeroRegistrado){
     LabelNombrePokemon.innerHTML = NumeroRegistrado;
 }
 
+function MostrarTipos(Datos){
+    let Tipos = Datos.types;
+    let Tipo1;
+    let Tipo2;
+    if (Tipos.length > 1){
+        if (Datos.types[0].type.name == "fire"){
+            Tipo1 = "Fuego";
+        }
+        else if (Datos.types[0].type.name == "water"){
+            Tipo1 = "Agua";
+        }
+        else if (Datos.types[0].type.name == "electric"){
+            Tipo1 = "Eléctrico";
+        }
+        else if (Datos.types[0].type.name == "grass"){
+            Tipo1 = "Planta";
+        }
+        else if (Datos.types[0].type.name == "ice"){
+            Tipo1 = "Hielo";
+        }
+        else if (Datos.types[0].type.name == "fighting"){
+            Tipo1 = "Luchador";
+        }
+        else if (Datos.types[0].type.name == "poison"){
+            Tipo1 = "Veneno";
+        }
+        else if (Datos.types[0].type.name == "ground"){
+            Tipo1 = "Tierra";
+        }
+        else if (Datos.types[0].type.name == "flying"){
+            Tipo1 = "Volador";
+        }
+        else if (Datos.types[0].type.name == "psychic"){
+            Tipo1 = "Psíquico";
+        }
+        else if (Datos.types[0].type.name == "bug"){
+            Tipo1 = "Bicho";
+        }
+        else if (Datos.types[0].type.name == "rock"){
+            Tipo1 = "Roca";
+        }
+        else if (Datos.types[0].type.name == "ghost"){
+            Tipo1 = "Fantasma";
+        }
+        else if (Datos.types[0].type.name == "normal"){
+            Tipo1 = "Normal";
+        }
+        else if (Datos.types[0].type.name == "dragon"){
+            Tipo1 = "Dragón";
+        }
+        else if (Datos.types[0].type.name == "dark"){
+            Tipo1 = "Siniestro";
+        }
+        else if (Datos.types[0].type.name == "steel"){
+            Tipo1 = "Acero";
+        }
+        else if (Datos.types[0].type.name == "fairy"){
+            Tipo1 = "Hada";
+        }
+
+        if (Datos.types[1].type.name == "fire"){
+            Tipo2 = "Fuego";
+        }
+        else if (Datos.types[1].type.name == "water"){
+            Tipo2 = "Agua";
+        }
+        else if (Datos.types[1].type.name == "electric"){
+            Tipo2 = "Eléctrico";
+        }
+        else if (Datos.types[1].type.name == "grass"){
+            Tipo2 = "Planta";
+        }
+        else if (Datos.types[1].type.name == "ice"){
+            Tipo2 = "Hielo";
+        }
+        else if (Datos.types[1].type.name == "fighting"){
+            Tipo2 = "Luchador";
+        }
+        else if (Datos.types[1].type.name == "poison"){
+            Tipo2 = "Veneno";
+        }
+        else if (Datos.types[1].type.name == "ground"){
+            Tipo2 = "Tierra";
+        }
+        else if (Datos.types[1].type.name == "flying"){
+            Tipo2 = "Volador";
+        }
+        else if (Datos.types[1].type.name == "psychic"){
+            Tipo2 = "Psíquico";
+        }
+        else if (Datos.types[1].type.name == "bug"){
+            Tipo2 = "Bicho";
+        }
+        else if (Datos.types[1].type.name == "rock"){
+            Tipo2 = "Roca";
+        }
+        else if (Datos.types[1].type.name == "ghost"){
+            Tipo2 = "Fantasma";
+        }
+        else if (Datos.types[1].type.name == "normal"){
+            Tipo2 = "Normal";
+        }
+        else if (Datos.types[1].type.name == "dragon"){
+            Tipo2 = "Dragón";
+        }
+        else if (Datos.types[1].type.name == "dark"){
+            Tipo2 = "Siniestro";
+        }
+        else if (Datos.types[1].type.name == "steel"){
+            Tipo2 = "Acero";
+        }
+        else if (Datos.types[1].type.name == "fairy"){
+            Tipo2 = "Hada";
+        }
+    }
+    else{
+        if (Datos.types[0].type.name == "fire"){
+            Tipo1 = "Fuego";
+        }
+        else if (Datos.types[0].type.name == "water"){
+            Tipo1 = "Agua";
+        }
+        else if (Datos.types[0].type.name == "electric"){
+            Tipo1 = "Eléctrico";
+        }
+        else if (Datos.types[0].type.name == "grass"){
+            Tipo1 = "Planta";
+        }
+        else if (Datos.types[0].type.name == "ice"){
+            Tipo1 = "Hielo";
+        }
+        else if (Datos.types[0].type.name == "fighting"){
+            Tipo1 = "Luchador";
+        }
+        else if (Datos.types[0].type.name == "poison"){
+            Tipo1 = "Veneno";
+        }
+        else if (Datos.types[0].type.name == "ground"){
+            Tipo1 = "Tierra";
+        }
+        else if (Datos.types[0].type.name == "flying"){
+            Tipo1 = "Volador";
+        }
+        else if (Datos.types[0].type.name == "psychic"){
+            Tipo1 = "Psíquico";
+        }
+        else if (Datos.types[0].type.name == "bug"){
+            Tipo1 = "Bicho";
+        }
+        else if (Datos.types[0].type.name == "rock"){
+            Tipo1 = "Roca";
+        }
+        else if (Datos.types[0].type.name == "ghost"){
+            Tipo1 = "Fantasma";
+        }
+        else if (Datos.types[0].type.name == "normal"){
+            Tipo1 = "Normal";
+        }
+        else if (Datos.types[0].type.name == "dragon"){
+            Tipo1 = "Dragón";
+        }
+        else if (Datos.types[0].type.name == "dark"){
+            Tipo1 = "Siniestro";
+        }
+        else if (Datos.types[0].type.name == "steel"){
+            Tipo1 = "Acero";
+        }
+        else if (Datos.types[0].type.name == "fairy"){
+            Tipo1 = "Hada";
+        }
+        Tipo2 = "";
+    }
+    const LabelTipo1 = document.getElementById("LabelTipo1");
+    LabelTipo1.innerHTML = Tipo1;
+    const LabelTipo2 = document.getElementById("LabelTipo2");
+    LabelTipo2.innerHTML = Tipo2;
+}
